21a69b016cfbf778a0c14898fbe40c3ed589de85
CryptoMatth <crypto.matth719@gmail.com>
2025-11-16 12:44:09 +0100

    Update owlNotifications.js

--- utils/owlNotifications.js
+++ utils/owlNotifications.js
@@ -88,7 +88,7 @@ function initializeOwl() {
 				//console.log('✅ Notifica NASCOSTA (tutti i messaggi letti)');
 			}
 		}
-	}, 1000);  // ← CAMBIA da 1000 a 3000 (3 secondi)
+	}, 100);  // ← CAMBIA da 1000 a 3000 (3 secondi)
 
 
 	// ===== EVENTO: Click sul gufo =====
