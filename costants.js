1722a41a20e1e20fe217d363b56e8470abfb897f
CryptoMatth <crypto.matth719@gmail.com>
2025-11-16 10:39:30 +0100

    Create costants.js

---
+++ costants.js
@@ -0,0 +1,66 @@
+// Stelle scintillanti
+const starContainer = document.getElementById('stars');
+for (let i = 0; i < 150; i++) {
+const star = document.createElement('div');
+	star.classList.add('star');
+	star.style.top = Math.random() * 100 + '%';
+	star.style.left = Math.random() * 100 + '%';
+	starContainer.appendChild(star);
+}
+
+// Fiocchi di neve
+const snowContainer = document.getElementById('snow');
+for (let i = 0; i < 50; i++) {
+	const flake = document.createElement('div');
+	flake.classList.add('flake');
+	flake.style.left = Math.random() * 100 + '%';
+	flake.style.animationDuration = 5 + Math.random() * 5 + 's';
+	flake.style.animationDelay = Math.random() * 5 + 's';
+	snowContainer.appendChild(flake);
+}
+
+// ===== DATI DEI MESSAGGI PERIODICI =====
+const PERIODIC_MESSAGES = {
+	day5_milestone: {
+		id: 'day5_milestone',
+		title: 'Dove sono le mie crocchette?',
+		description: 'Perché non mi hai portato le crocchette? Pensi che io sia culone?<br><strong>OK!</strong><br>Li avevo creati ma non ero sicuro di farteli fare, ma ora sono arrabbiato e quindi dovrai eseguire tutti i test!<br>Scendi in fondo alla pagina e inizia.<br>Però sono gentile: dl test puoi prendere un massimo di 3 stelle: se arrivi a un minimo di 10 punti totali, otterrai un regalino!<br>A più tardi, e spero con delle crocchette',
+		image: 'foto/Sherlock_babbo_natale.png'
+	},
+	day10_milestone: {
+		id: 'day10_milestone',
+		title: 'Sorpresa',
+		description: 'Ti sono mancato? Ti ricordavi di me?<br>Cicci è un po\' arrabbiato, cosa ci devi fare? Tornerà, ora vado a recuperare delle crocchette da parte tua.<br>Però ormai ti tocca eseguire tutti i test. Dai, se sei arrivata fin qui stai diventando brava!<br>A presto.',
+		image: 'foto/oke_natalizio.png'
+	},
+	day15_milestone: {
+		id: 'day15_milestone',
+		title: 'Ancora Oké',
+		description: 'Ti scrivo velocemente, sono preso con tutti questi regali da confezionare. Chissà se fra questi c\'è anche il tuo?<br>Comunque non ho ancora avuto il tempo di dare le crocchette a Cicci, prometto che gliele darò appena possibile. Lui è un pezzo grosso ormai (guardia del corpo della famiglia di Babbo Natale).<br>Ricorda: se ottieni un minimo di 30 punti totali, otterrai un altro regalino!<br>Buon test!',
+		image: 'foto/oke_regali.png'
+	},
+	day20_milestone: {
+		id: 'day20_milestone',
+		title: 'Crocchette?',
+		description: 'Oké mi ha portato le crocchette da parte tua, ok. Allora va bene.<br>Dai, ti farò questo test più facile. MANCA POCO A NATALEEEEEEE.<br>Come regalo, guarda questa foto di Oké che costruisce un piccolo Babbo Natale.',
+		image: 'foto/oke_babbo_natale.png'
+	},
+	day25_milestone: {
+		id: 'day25_milestone',
+		title: 'NATALEEEEEEEE',
+		description: 'E\' NATALEEEEEE!<br>Che giornata pesante, ma spero che sarai felice quanto lo siamo noi. Mattia è bravo con te? Lo tengo d\'occhio, eh.<br>Quasi quasi non ti faccio fare i test...<br>...<br>Naaah, scherzo. Buon Natale!<br>E mi raccomando: se vuoi l\'ultimo dei tre regalini natalizi, devi ottenere un minimo di 50 punti totali!',
+		image: 'foto/sherlock_oke_natalizio.png'
+	},
+	day30_milestone: {
+		id: 'day30_milestone',
+		title: 'Ci siamo quasi!',
+		description: 'Conoscendoti non saresti così felice: il Natale è fintio, il compleanno quasi, tra un po\' toccherà pensare al regalo per Mattia. Che brutto periodo.<br>Spero almeno che questo clanedario, così duramente preparato da entrambi, ti possa aver dato un po\' di felicità. Felicità, proprio come il test che ora devi fare.<br>Buon divertimento!',
+		image: 'foto/sherlock_oke_compleanno.png.png'
+	},
+	day31_milestone: {
+		id: 'day31_milestone',
+		title: 'TANTISSIMI AUGURI!!',
+		description: 'Tanti auguri a te, tanti auguri a te, le crocchette a meeeee...<br>Sherlock, non devi essere egocentrico. E\' il compleanno di Ale!<br>Sì, e le crocchette a me.<br><br>Speriamo che questo calendario ti sia piaciuto, ci siamo impegnati tantissimo. Ti vogliamo tanto, tanto bene. Tantissimi auguri di buon compleanno!',
+		image: 'foto/sherlock_oke_compleanno_ultimo.png'
+	},
+};
